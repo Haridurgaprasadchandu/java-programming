a8b42d5e74b1db092fc67751ffd0dbca18db090c
Hari durga prasad .Chandu <106144826+Haridurgaprasadchandu@users.noreply.github.com>
2022-07-26 13:54:58 +0530

    Add files via upload

---
+++ alleven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class alleven
+  {
+    public static void main(String args[])
+    {
+     int i=1;
+      System.out.println("Even No's upto n");
+      while(i<=100)
+        {
+           if(i%2==0)
+           {
+                  System.out.println(i);
+           }
+          i=i+1;
+        }
+     
+    }    
+  }
